08aa63b24955d8e1ac08231501335533ac2cd04d
cympyc1785 <cympyc1785@gmail.com>
2023-02-23 03:22:53 +0900

    Apply Background Material.

--- Assets/Scripts/UI/SettingData.cs
+++ Assets/Scripts/UI/SettingData.cs
@@ -7,6 +7,7 @@ public class SettingData : MonoBehaviour
     public float mainVolume = 1f, bgmVolume = 1f, sfxVolume = 1f;
     public Color answerDrawingColor = Color.white;
     public Material BGMaterial = null;
+    public Vector3 BGScale = new Vector3(3, 1, 3);
 
     public static SettingData Instance;
     
